349ae08451fbdeecf261283871e7bda8c4ee9e3f
James Landry <james.j.landry@outlook.com>
2020-05-22 18:41:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,9 @@
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), array|
-      
+    value.each do |inside_key, names|
+      names.each do |name|
+    end
+  end
+    array 
    end
 end
