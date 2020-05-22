e6278029416197ee37d595e26eee01038632956b
James Landry <james.j.landry@outlook.com>
2020-05-22 18:35:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,5 @@
 def nyc_pigeon_organizer(data)
-  first_hash = {}
-  second_hash = {}
-  new_hash = {}
-  data.map do |key, value|
-      first_hash = value
-    end
-  first_hash.map do |key, value|
-    second_hash = value
-    end
-    second_hash.each_with_object do |key, hash|
+  data.each_with_object({}) do |(key, value), array|
       
    end
 end
